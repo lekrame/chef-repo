50c60b7c216eb3a5686acb0b5b0a39b04bb2e6d3
Michael Kramer <git@pianospree.com>
2016-09-29 17:17:43 -0700

    fixes

--- kstuff/recipes/kramercmpx2.rb
+++ kstuff/recipes/kramercmpx2.rb
@@ -1,27 +1,37 @@
 directory '/tmp/message'
 
-file "/tmp/message/motd" do
-	content "\n\nInstance started at " + Time.new.inspect + "\n\
-	Node IP address = " + #{node['ipaddress']} + " running " \
-	#{node['platform']}\n\n"
+file '/tmp/message/motd' do
+  content "\n\nInstance started at #{Time.new.inspect}\n\
+  Node IP address = #{node['ipaddress']} running #{node['platform']}"
+  action :create
+  mode '0777'
 end
 
-file "~/startup.bash" do
-	content "#!/bin/bash\
-	# peekaboo
-	touch /tmp/touchme\
-	touch /tmp/myjunk
-	"
-	mode '0777'
+# file '/tmp/message/motd' do
+#   content "\n\nInstance started at #{Time.new.inspect} + "\n\
+#   Node IP address = " + #{node['ipaddress']} +
+#  " running #{node['platform']}\n\n"}
+#   action :create
+#   mode '0777'
+# end
+
+file '~/startup.bash' do
+  content "#!/bin/bash\
+  # peekaboo
+  touch /tmp/touchme\
+  touch /tmp/myjunk
+  "
+  action :create
+  mode 0777
 end
 
-execute "start-this-thing" do
-	command '~/startup.bash'
+execute 'start-this-thing' do
+  command '~/startup.bash'
 end
 
 Chef::Log.info('did Kramer get logged?')
 
-execute "message-of-the-day" do
-	command 'cp /tmp/message/motd /etc'
-	ignore_failure :true
+execute 'message-of-the-day' do
+  command 'cp /tmp/message/motd /etc'
+  ignore_failure true
 end
