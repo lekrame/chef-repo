f624d9d386e218a34a5c6f74d509c310aede69f7
Michael Kramer <git@pianospree.com>
2016-10-01 01:09:45 -0700

    restructure with cookbooks di

--- kstuff/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'kstuff'
-maintainer       'Michael Kramer'
-maintainer_email 'chef@pianospree.com'
-license          'All rights reserved'
-description      'Installs/Configures kstuff'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
