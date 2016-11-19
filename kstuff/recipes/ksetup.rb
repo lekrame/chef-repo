e2f6d091f3c11b977a42424f4a57af21e8209a53
Michael Kramer <git@pianospree.com>
2016-11-19 09:54:38 -0800

    change HERE text to quoted string

--- kstuff/recipes/ksetup.rb
+++ kstuff/recipes/ksetup.rb
@@ -7,7 +7,9 @@ apt_package 'awscli' do
 end
 
 bash do
-	code <<-EOH'
+	code 'sudo apt-add-repository ppa:brightbox/ruby-ng
+	sudo apt-get update
+	sudo apt-get install ruby2.2
 	sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.2 400 \
 	 --slave /usr/bin/rake rake /usr/bin/rake2.2 \
 	 --slave /usr/bin/ri ri /usr/bin/ri2.2 \
@@ -19,8 +21,7 @@ bash do
 	 --slave /usr/share/man/man1/ri.1.gz ri.1.gz /usr/share/man/man1/ri2.2.1.gz \
 	 --slave /usr/share/man/man1/rdoc.1.gz rdoc.1.gz /usr/share/man/man1/rdoc2.2.1.gz \
 	 --slave /usr/share/man/man1/gem.1.gz gem.1.gz /usr/share/man/man1/gem2.2.1.gz \
-	 --slave /usr/share/man/man1/irb.1.gz irb.1.gz /usr/share/man/man1/irb2.2.1.gz
-	 '-EOH
+	 --slave /usr/share/man/man1/irb.1.gz irb.1.gz /usr/share/man/man1/irb2.2.1.gz'
 end
 
 #apt_package 'ruby' do
