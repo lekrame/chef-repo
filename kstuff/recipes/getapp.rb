d0c2d2c9b121ab8b67ba14907d311da147c49c9d
Michael Kramer <git@pianospree.com>
2016-11-19 16:17:07 -0800

    work?

--- kstuff/recipes/getapp.rb
+++ kstuff/recipes/getapp.rb
@@ -1,3 +1,6 @@
-directory '~/kramer'
-directory '~/kramer/apps'
-#git 
+directory '/home/ubuntu/kramer'
+directory '/home/ubuntu/kramer/apps'
+git 'server' do
+	destination '/home/ubuntu/kramer/apps'
+	repository 'lekrame:asdfjk1g@//github.com/lekrame/chef-repo/kstuff/recipes.git'
+end
