c7d3a60a218a49c8fb2a39ae7b9145f41a1b3475
Michael Kramer <git@pianospree.com>
2016-11-19 17:00:29 -0800

    allow alternative to fail

--- kstuff/recipes/ksetup.rb
+++ kstuff/recipes/ksetup.rb
@@ -19,7 +19,7 @@ bash 'ini2' do
 end
 
 bash 'init' do
-#	ignore_failure true
+	ignore_failure true
 	code 'sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.2 400 \
 	 --slave /usr/bin/rake rake /usr/bin/rake2.2 \
 	 --slave /usr/bin/ri ri /usr/bin/ri2.2 \
--- kstuff/recipes/simple.rb
+++ kstuff/recipes/simple.rb
@@ -1,7 +1,12 @@
-directory '/tmp/message'
-file '/tmp/message/tst'
-bash "tst00" do
-	code <<-EOT
-		touch /tmp/myjunk2
-		EOT
-end
+#directory '/tmp/message'
+#file '/tmp/message/tst'
+#bash "tst00" do
+#	code <<-EOT
+#		touch /tmp/myjunk2
+#		EOT
+#end
+
+#bash "tst00" do code 'touch /tmp/myjunk3' end
+#bash "tst00" do 
+#	code 'touch /tmp/myjunk3'
+#end
