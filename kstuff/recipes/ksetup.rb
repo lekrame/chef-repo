d8729ea8dae3bda2dc7a1d6eaad5a5e5f8f68772
Michael Kramer <git@pianospree.com>
2016-12-09 13:45:39 -0800

    commented gem lines to end of file

--- kstuff/recipes/ksetup.rb
+++ kstuff/recipes/ksetup.rb
@@ -33,12 +33,12 @@ bash 'init' do
 	 --slave /usr/bin/rake rake /usr/bin/rake2.2 \
 	 --slave /usr/bin/ri ri /usr/bin/ri2.2 \
 	 --slave /usr/bin/rdoc rdoc /usr/bin/rdoc2.2 \
-#	 --slave /usr/bin/gem gem /usr/bin/gem2.2 \
 	 --slave /usr/bin/irb irb /usr/bin/irb2.2 \
 	 --slave /usr/share/man/man1/ruby.1.gz ruby.1.gz /usr/share/man/man1/ruby2.2.1.gz \
 	 --slave /usr/share/man/man1/rake.1.gz rake.1.gz /usr/share/man/man1/rake2.2.1.gz \
 	 --slave /usr/share/man/man1/ri.1.gz ri.1.gz /usr/share/man/man1/ri2.2.1.gz \
 	 --slave /usr/share/man/man1/rdoc.1.gz rdoc.1.gz /usr/share/man/man1/rdoc2.2.1.gz \
-#	 --slave /usr/share/man/man1/gem.1.gz gem.1.gz /usr/share/man/man1/gem2.2.1.gz \
 	 --slave /usr/share/man/man1/irb.1.gz irb.1.gz /usr/share/man/man1/irb2.2.1.gz'
 end
+#	 --slave /usr/bin/gem gem /usr/bin/gem2.2 \
+#	 --slave /usr/share/man/man1/gem.1.gz gem.1.gz /usr/share/man/man1/gem2.2.1.gz \
