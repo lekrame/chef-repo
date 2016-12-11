5e33525cb3a1a80e415f535030c41e4eda5a18f8
Michael Kramer <git@pianospree.com>
2016-12-10 22:22:50 -0800

    oops

--- kstuff/recipes/start.rb
+++ kstuff/recipes/start.rb
@@ -3,7 +3,7 @@ apt_update 'update' do
 end
 
 bash 'ini0' do
-	code ''yes "" |sudo apt-add-repository ppa:brightbox/ruby-ng'
+	code 'yes "" |sudo apt-add-repository ppa:brightbox/ruby-ng'
 end
 
 bash 'ini1' do
