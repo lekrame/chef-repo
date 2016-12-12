68819cf46d321014f3a191fbbfde8a883e66341e
Michael Kramer <git@pianospree.com>
2016-12-11 22:55:43 -0800

    fix

--- kstuff/recipes/start.rb
+++ kstuff/recipes/start.rb
@@ -1,14 +1,14 @@
-apt_update 'update' do
-	action :update
-end
-
 bash 'ini0' do
 	code 'sudo yes "" | sudo apt-add-repository ppa:brightbox/ruby-ng'
 end
 
-bash 'ini1' do
-	code 'sudo apt-get update'
+apt_update 'update' do
+	action :update
 end
+
+#bash 'ini1' do
+#	code 'sudo apt-get update'
+#end
 
 bash 'ini1.1' do
 	code 'sudo apt-get upgrade'
